f7d5f6d2c892c5eb7ff59b74ead60128215a02f1
Laura Navarro <lvna.95@gmail.com>
2024-11-14 17:44:31 -0500

    Setup

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,12 +1,33 @@
 import React from 'react';
+import { FaReact, FaNodeJs, FaDatabase, FaPython, FaGithub, FaTools } from 'react-icons/fa'; 
+
+import './About.css';  
 
 const About = () => {
   return (
-    <div>
-      <h1>Sobre Mí</h1>
-      <p>Contenido de la página sobre mí.</p>
+    <div className="about-container">
+      <h1 className="about-heading">Sobre Mí</h1>
+      <p className="about-paragraph">
+        ¡Hola! Soy Laura Valeria Navarro Avellaneda, una desarrolladora Front-End con más de 3 años de experiencia en la construcción de aplicaciones web utilizando tecnologías como ReactJS, TypeScript, Firebase y NodeJS. Me apasiona crear soluciones eficientes, escalables y de alta calidad.
+      </p>
+      
+      <h2 className="about-subheading">Habilidades Técnicas</h2>
+      <ul className="about-list">
+        <li><FaReact /> Frontend: ReactJS, Redux, HTML5, CSS3, Sass, JavaScript (ES6+), TypeScript</li>
+        <li><FaNodeJs /> Backend: NodeJS, Express</li>
+        <li><FaDatabase /> Bases de datos: Firebase</li>
+        <li><FaPython /> Automatización: Python </li>
+        <li><FaTools /> Herramientas y metodologías: Git, GitHub, Visual Studio Code, Agile (Scrum)</li>
+      </ul>
+
+      <h2 className="about-subheading">Proyectos y GitHub</h2>
+      <p className="about-paragraph">
+        Puedes encontrar más detalles sobre mis proyectos y contribuciones en mi perfil de GitHub: 
+        <a href="https://github.com/lauravalnavarro" target="_blank" rel="noopener noreferrer" className="about-link"><FaGithub /> lauravalnavarro</a>.
+      </p>
     </div>
   );
 }
 
 export default About;
+
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -7,28 +7,22 @@ const Home = () => {
     <div className="hero-container">
       <div className="hero-text">
         <h1 className="hero-title">
-          <span className="typed-text">Bienvenido a Mi Portafolio</span>
+          <span className="typed-text">Bienvenid@ a Mi Portafolio</span>
         </h1>
-        <div className="hero-image">
         <p className="hero-description">
           Soy Laura Valeria Navarro, una desarrolladora apasionada por la programación y el aprendizaje. 
           Explora mis proyectos y conoce más sobre mi experiencia.
         </p>
-        <img src="https://media.licdn.com/dms/image/C4D03AQEaDtTEz3Pw5A/profile-displayphoto-shrink_800_800/0/1580419211289?e=1729123200&v=beta&t=B6zUlX_JKgSh29cPJn3C0MFeGAQ2ztIjmdBNwVt0W5o" alt="Foto de Laura Valeria Navarro" />
-      </div>
       </div>
     </div>
-
-  <section id="projects" className="projects-section">
-    <h2>Proyectos Destacados</h2>
-    <Mosaic/>
-  </section>
-
-
-  <section id="contact" className="contact-section">
-    <h2>¡Conectemos!</h2>
-    <p>¿Interesado en trabajar conmigo? No dudes en <a href="mailto:tuemail@example.com">enviarme un correo electrónico</a>.</p>
-  </section>
+    <section id="projects" className="projects-section">
+      <h2>Proyectos Destacados</h2>
+      <Mosaic/>
+    </section>
+    <section id="contact" className="contact-section">
+      <h2>¡Conectemos!</h2>
+      <p>¿Interesado en trabajar conmigo? No dudes en <a href="mailto:tuemail@example.com">enviarme un correo electrónico</a>.</p>
+    </section>
 </div>
 
   );
